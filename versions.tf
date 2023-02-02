e48c78db259ec5977a63d3c14464f807956c71d0
Markus <markus.okafor@gmail.com>
2023-02-02 19:02:54 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,11 +1,17 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.66.0"
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
     }
   }
-
-  required_version = ">= 0.14"
 }
 
+provider "azurerm" {
+  features {}
+
+  subscription_id   = ${env.ARM_SUBSCRIPTION_ID}
+  tenant_id         = ${env.ARM_TENANT_ID}
+  client_id         = ${env.ARM_CLIENT_ID}  
+  client_secret     = ${env.ARM_CLIENT_SECRET}
+}
